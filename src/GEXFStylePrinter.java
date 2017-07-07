754dc182885bf7096bc9d5924ccaa7f359ddda41
Owner <Owner@157.80.164.54>
2017-07-07 13:33:48 +0900

    GEXFStylePloter

---
+++ src/GEXFStylePrinter.java
@@ -0,0 +1,18 @@
+
+public class GEXFStylePrinter {
+	Network net;
+	
+	public GEXFStylePrinter(Network net) {
+		this.net = net;
+	}
+	
+	public void init(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/テスト用.java
+++ src/テスト用.java
@@ -6,15 +6,27 @@ public class テスト用 {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
-		ArrayList<String> x = new ArrayList<String>();
+//		ArrayList<String> x = new ArrayList<String>();
+//		
+//		for(int i=0;i<10;i++) x.add(Character.toString(((char)('a'+i))));
+//		x.add(0, "あああ");
+//		
+//		for(int i=0;i<x.size();i++){
+//			System.out.println(x.get(i));
+//		}
 		
-		for(int i=0;i<10;i++) x.add(Character.toString(((char)('a'+i))));
-		x.add(0, "あああ");
+		method(1);
+		String s = "";
+		method(0);
+	}
+	
+	static void method(Object o){
+		if(o.toString().length() == 0)System.out.println("null");
+		else System.out.println("テスト:"  + o);
 		
-		for(int i=0;i<x.size();i++){
-			System.out.println(x.get(i));
+		if(o.equals(Integer.class)){
+			System.out.println(o + "is Int");
 		}
-		
 	}
 
 }
