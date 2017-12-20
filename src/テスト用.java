c87e2ad6b883dabc6c2c41e1ad22e21657854dc0
Owner <Owner@OptiPlex7050.mshome.net>
2017-12-20 12:40:25 +0900

    RegularRandom

--- src/テスト用.java
+++ src/テスト用.java
@@ -1,34 +1,15 @@
 public class テスト用{
 	public static void main(String[] args) throws Exception{
 		int N = 1000;
-		MakePowerLaw dist = new MakePowerLaw(N, 2.7);
-//		ConfigrationNetwork net = new ConfigrationNetwork(dist.degree, 100);
-//		while(!net.success){
-//			dist = new MakePowerLaw(1000, 2.7);
-//			net = new ConfigrationNetwork(dist.degree, 100);
-//		}
-		RandomNetwork net = new RandomNetwork(N, dist.averageDegree()/N);
+		int d = 5;
+		int[] degree = new int[N];
+		for(int i=0;i<N;i++) degree[i]=d;
 		
-		int[] hist = new int[N+1];
-		for(int i=0;i<net.N;i++){
-			hist[net.degree[i]]++;
-		}
+		ConfigrationNetwork net = new ConfigrationNetwork(degree, 100);
 		
-		double inv_N = 1.0/N;
-		double cum = 0;
-		for(int i=0;i<hist.length;i++){
-			if(hist[i]>0){
-//				{
-//					cum += hist[i]*inv_N;
-//					System.out.println(i+"\t"+(1.0-cum));
-//				}
-				
-				{
-					System.out.println(i + "\t" + hist[i]*inv_N);
-				}
-				
-			}
-		}
+		net.sort();
+		net.printList();
+		net.printList("RegularRandom_N1000degree5.csv");
 
 	}
 }
