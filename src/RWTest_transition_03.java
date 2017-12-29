aed16b2f958613dd3803e160f945c3f14cf33364
Owner <Owner@home-PC>
2017-12-29 15:37:10 +0900

    fix

--- src/RWTest_transition_03.java
+++ src/RWTest_transition_03.java
@@ -7,25 +7,25 @@ public class RWTest_transition_03 {
 		int stepN = N*1000;
 		double deltaW = 1.0;
 		RandomNetwork net = new RandomNetwork(N, p);
-		
+
 		net.setNode(false);
 		net.setEdge();
-		
+
 		double[] vWeight;
 		vWeight = net.VertexReinforcedRandomWalk(stepN, deltaW);
-		
+
 		int[] vWeightN = new int[N];
 		for(int i=0;i<vWeight.length;i++){
 			vWeightN[i] = (int)Math.round((vWeight[i]-1)/deltaW);
 		}
-		
+
 		int[][] sortedVWeightN;
 		Comprator2dim sorter = new Comprator2dim();
-		sortedVWeightN = sorter.sort(vWeightN, 1, false);
-		
+		sortedVWeightN = sorter.sort(vWeightN, false);
+
 		for(int i=0;i<vWeight.length;i++){
 			System.out.println(sortedVWeightN[i][0] + "\t" + sortedVWeightN[i][1]);
 		}
-		
+
 	}
 }
--- src/テスト用.java
+++ src/テスト用.java
@@ -7,6 +7,7 @@ public class テスト用{
 		double averageSize = 0.0;
 		for(int i=0;i<sizeDistribution.length;i++) {
 			averageSize += i*sizeDistribution[i];
+			System.out.println(sizeDistribution[i]);
 		}
 
 
