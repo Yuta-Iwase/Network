33b744e3c6fd97357d291ea20d8ad57d4da528d3
Yuta-Iwase <yutabk201@gmail.com>
2018-01-29 14:09:14 +0900

    aa

--- src/テスト用.java
+++ src/テスト用.java
@@ -6,55 +6,58 @@ public class テスト用{
 		int sumDegree;
 		int cc = 0;
 		Network net;
-		
-		
-//		do {
-//			do {
-//				dist = new MakePowerLaw(N, 1.05, 1, 10);
-//				sumDegree = 0;
-//				for(int i=0;i<N;i++) {
-//					sumDegree += dist.degree[i];
-//				}
-//				sumDegree /= 2;
-//			}while(sumDegree != 215);
-//
-//
-//			int limit = 0;
-//			do {
-//				net = new ConfigrationNetwork(dist.degree, 100,false);
-//			}while(!net.success && (limit++)<100);
-//
-//			if(net.success) {
-//				net.setNode(false);
-//				net.setEdge();
-//				net.ConnectedCompornent();
-//				cc = net.count_cc;
-//			}else {
-//				cc = 0;
-//			}
-//
-//			System.out.println(net.count_cc);
-//
-//		}while(net.count_cc != 1);
+
 
 		do {
-			net = new RandomNetwork(N,0.02564102564102564);
+			do {
+				dist = new MakePowerLaw(N, 2.7, 2, N-1);
+				sumDegree = 0;
+				for(int i=0;i<N;i++) {
+					sumDegree += dist.degree[i];
+				}
+				sumDegree /= 2;
+			}while(sumDegree != 258);
 
-			net.setNode(false);
-			net.setEdge();
-			net.ConnectedCompornent();
-			
-			System.out.println(net.M);
-			if(net.M==215) {
-				System.out.println(net.count_cc);
+
+			int limit = 0;
+			do {
+				net = new ConfigrationNetwork(dist.degree, 100,false);
+			}while(!net.success && (limit++)<100);
+
+			if(net.success) {
+				net.setNode(false);
+				net.setEdge();
+				net.ConnectedCompornent();
+				cc = net.count_cc;
+			}else {
+				cc = 0;
 			}
 
-		}while(net.M!=215 || net.count_cc!=1);
+			System.out.println(net.count_cc);
+
+		}while(net.count_cc != 1);
+
+//		do {
+//			net = new RandomNetwork(N,0.03);
+//
+//			net.setNode(false);
+//			net.setEdge();
+//			net.ConnectedCompornent();
+//			System.out.println(net.count_cc);
+//
+////			System.out.println(net.M);
+////			if(net.M==215) {
+////				System.out.println(net.count_cc);
+////			}
+////
+////		}while(net.M!=215 || net.count_cc!=1);
+//		}while(net.count_cc!=1);
+////		}while(false);
 
 
-
-		String fileName = "random.csv";
-		net.printList("C:\\Users\\Owner\\Desktop\\" + fileName);
+		String fileName = "sfn.csv";
+//		net.printList("C:\\Users\\Owner\\Desktop\\" + fileName);
+		net.printList("" + fileName);
 		System.out.println(net.count_cc);
 
 	}
