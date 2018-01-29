322a5632595572349d71c452e18cde3b3d1ff11e
Owner <yutabk201@gmail.com>
2018-01-29 18:02:09 +0900

    cl

--- src/ClusteringConfigrationNetwork.java
+++ src/ClusteringConfigrationNetwork.java
@@ -12,33 +12,48 @@ import java.util.Random;
 
 public class ClusteringConfigrationNetwork extends Network{
 
-	public ClusteringConfigrationNetwork(int[] isolated_DegreeList,int loopLimit) {
-		generate(degree, loopLimit);
+	public ClusteringConfigrationNetwork(int[] isolated_DegreeList,int[] clustering_DegreeList,int loopLimit) {
+		generate(isolated_DegreeList,clustering_DegreeList, loopLimit, true);
 	}
 
-	public ClusteringConfigrationNetwork(int[] isolated_DegreeList,int loopLimit,boolean message){
-		generate(degree, loopLimit, message);
+	public ClusteringConfigrationNetwork(int[] isolated_DegreeList,int[] clustering_DegreeList,int loopLimit,boolean message){
+		generate(isolated_DegreeList,clustering_DegreeList, loopLimit, message);
 	}
 
-	private void generate(int[] degree,int loopLimit,boolean message) {
+	private void generate(int[] isolated_DegreeList,int[] clustering_DegreeList,int loopLimit,boolean message) {
 		directed = false;
 		doubleCount = false;
-		N = degree.length;
+		N = isolated_DegreeList.length;
 		this.degree = new int[N];
 
-		ArrayList<Integer> array = new ArrayList<Integer>();
+		ArrayList<Integer> isolatedStubList = new ArrayList<Integer>();
+		ArrayList<Integer> clusteringStubList = new ArrayList<Integer>();
+		int[] clusteringStubremainder = new int[N];
+		int sum_isolatedStub = 0;
+		int sum__clusteringStub = 0;
 		int sumDegree=0;
 		for(int i=0;i<N;i++){
-			for(int j=0;j<degree[i];j++){
-				array.add(i);
+			this.degree[i] = 0;
+
+			for(int j=0;j<isolated_DegreeList[i];j++){
+				isolatedStubList.add(i);
 			}
-			this.degree[i] = degree[i];
-			sumDegree += degree[i];
+			this.degree[i] += isolated_DegreeList[i];
+			sum_isolatedStub += isolated_DegreeList[i];
+
+			for(int j=0;j<clustering_DegreeList[i];j++){
+				clusteringStubList.add(i);
+			}
+			this.degree[i] += clustering_DegreeList[i];
+			sum__clusteringStub += clustering_DegreeList[i];
+
+			clusteringStubremainder[i] = clustering_DegreeList[i];
 		}
+		sumDegree = sum_isolatedStub + sum__clusteringStub;
 		M = sumDegree/2;
 
 		Random rnd = new Random();
-		int disconnectedN=sumDegree;
+		int disconnectedN=sum_isolatedStub;
 		int targetEdgeA,targetEdgeB;
 		int nowLine=0;
 		int nowLoopLimit;
@@ -58,11 +73,11 @@ public class ClusteringConfigrationNetwork extends Network{
 				}
 				nowLoopLimit--;
 
-				selfLoop= array.get(targetEdgeA)==array.get(targetEdgeB);
+				selfLoop= isolatedStubList.get(targetEdgeA)==isolatedStubList.get(targetEdgeB);
 				multiple=false;
 				cheakMultiple:for(int i=0;i<nowLine;i++){
-					if((array.get(targetEdgeA)==list[i][0]&&array.get(targetEdgeB)==list[i][1])||
-					   (array.get(targetEdgeA)==list[i][1]&&array.get(targetEdgeB)==list[i][0])){
+					if((isolatedStubList.get(targetEdgeA)==list[i][0]&&isolatedStubList.get(targetEdgeB)==list[i][1])||
+					   (isolatedStubList.get(targetEdgeA)==list[i][1]&&isolatedStubList.get(targetEdgeB)==list[i][0])){
 						multiple=true;
 						break cheakMultiple;
 					}
@@ -70,15 +85,70 @@ public class ClusteringConfigrationNetwork extends Network{
 			}while(selfLoop || multiple);
 
 			if(targetEdgeA >= targetEdgeB){
-				list[nowLine][1]=array.get(targetEdgeA);
-				array.remove(targetEdgeA);
-				list[nowLine][0]=array.get(targetEdgeB);
-				array.remove(targetEdgeB);
+				list[nowLine][1]=isolatedStubList.get(targetEdgeA);
+				isolatedStubList.remove(targetEdgeA);
+				list[nowLine][0]=isolatedStubList.get(targetEdgeB);
+				isolatedStubList.remove(targetEdgeB);
 			}else{
-				list[nowLine][1]=array.get(targetEdgeB);
-				array.remove(targetEdgeB);
-				list[nowLine][0]=array.get(targetEdgeA);
-				array.remove(targetEdgeA);
+				list[nowLine][1]=isolatedStubList.get(targetEdgeB);
+				isolatedStubList.remove(targetEdgeB);
+				list[nowLine][0]=isolatedStubList.get(targetEdgeA);
+				isolatedStubList.remove(targetEdgeA);
+			}
+
+			disconnectedN -= 2;
+			nowLine++;
+		}while(disconnectedN>0);
+
+		int baseNodeOfCluster_relIndex;
+		int baseNodeOfCluster_absIndex;
+		disconnectedN=sum__clusteringStub;
+		nowLine=0;
+		success = true;
+		generateLoop: do{
+			nowLoopLimit=loopLimit;
+			do{
+				// TODO
+				baseNodeOfCluster_relIndex = rnd.nextInt(disconnectedN);
+				baseNodeOfCluster_absIndex = clusteringStubList.get(baseNodeOfCluster_relIndex);
+
+				if(nowLoopLimit<=0){
+					if(message) System.out.println("生成に失敗しました。");
+					success = false;
+					break generateLoop;
+				}
+				nowLoopLimit--;
+
+//				selfLoop= isolatedStubList.get(targetEdgeA)==isolatedStubList.get(targetEdgeB);
+				multiple=false;
+//				int
+				boolean malti_Base_A,malti_Base_B,malti_A_B;
+				cheakMultiple:for(int i=0;i<nowLine;i++){
+//					int node_a = ;
+//					malti_Base_A = ((clusteringStubList.get(targetEdgeA)==list[i][0]&&clusteringStubList.get(targetEdgeB)==list[i][1])||
+//							   (clusteringStubList.get(targetEdgeA)==list[i][1]&&clusteringStubList.get(targetEdgeB)==list[i][0]));
+
+
+
+
+					if((isolatedStubList.get(targetEdgeA)==list[i][0]&&isolatedStubList.get(targetEdgeB)==list[i][1])||
+					   (isolatedStubList.get(targetEdgeA)==list[i][1]&&isolatedStubList.get(targetEdgeB)==list[i][0])){
+						multiple=true;
+						break cheakMultiple;
+					}
+				}
+			}while(multiple);
+
+			if(targetEdgeA >= targetEdgeB){
+				list[nowLine][1]=isolatedStubList.get(targetEdgeA);
+				isolatedStubList.remove(targetEdgeA);
+				list[nowLine][0]=isolatedStubList.get(targetEdgeB);
+				isolatedStubList.remove(targetEdgeB);
+			}else{
+				list[nowLine][1]=isolatedStubList.get(targetEdgeB);
+				isolatedStubList.remove(targetEdgeB);
+				list[nowLine][0]=isolatedStubList.get(targetEdgeA);
+				isolatedStubList.remove(targetEdgeA);
 			}
 
 			disconnectedN -= 2;
@@ -86,8 +156,8 @@ public class ClusteringConfigrationNetwork extends Network{
 		}while(disconnectedN>0);
 	}
 
-	private void generate(int[] degree,int loopLimit) {
-		generate(degree, loopLimit, true);
+	private void generate(int[] isolated_DegreeList,int[] clustering_DegreeList,int loopLimit) {
+		generate(isolated_DegreeList,clustering_DegreeList, loopLimit, true);
 	}
 
 }
