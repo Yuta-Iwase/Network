bf862e58f0e164a48d654400d48a6f440f5e8db2
Owner <yutabk201@gmail.com>
2018-02-02 16:39:44 +0900

    aa

--- src/テスト用.java
+++ src/テスト用.java
@@ -1,44 +1,28 @@
+import java.io.File;
+import java.io.PrintWriter;
+
 public class テスト用{
 	public static void main(String[] args) throws Exception{
+		int N = 10000;
 		Network net;
 
+//		do {
+//			MakePowerLaw dist = new MakePowerLaw(N, 2.7, 2, N/10);
+//			net = new ConfigrationNetwork(dist.degree,100,false);
+//		}while(net.success);
 
-
-
-		Network net2;
 		do {
+			MakePoisson dist = new MakePoisson(N, 3.84406);
+			net = new ConfigrationNetwork(dist.degree,100,false);
+		}while(net.success);
 
-			MakePoisson dist1 = new MakePoisson(1000, 3.0);
-			MakePoisson dist2 = new MakePoisson(1000, 2.0);
-
-			net = new ClusteringConfigrationNetwork(dist1.degree, dist2.degree, 100,false);
-			
-			dist1 = new MakePoisson(1000, 8.0);
-			net2 = new ConfigrationNetwork(dist1.degree, 100);
-		}while(!net.success);
-
-//		net.printList();
-		net.printList("C:\\Desktop\\csv.csv");
-
-		net.setNode(false);
-		net2.setNode(false);
-
-		System.out.println("aa");
-		int t = 100;
-		for(int i=0;i<t;i++) {
-			double f = i/(t*1.0);
-			net.SitePercolation2018(f, true);
-			net.ConnectedCompornentNDI(true, false, false);
-//			System.out.println(net.maxCC_NID);
-			
-			net2.SitePercolation2018(f, true);
-			net2.ConnectedCompornentNDI(true, false, false);
-//			System.out.println(net2.maxCC_NID);
-			
-			System.out.println(f + "\t" + net.maxCC_NID + "\t" + net2.maxCC_NID);
+		PrintWriter pw = new PrintWriter(new File("C:\\desktop\\rnd.csv"));
+		for(int i=0;i<net.degree.length;i++) {
+			System.out.println(net.degree[i]);
+			pw.println(net.degree[i]);
 		}
 
-
+		pw.close();
 
 
 	}
