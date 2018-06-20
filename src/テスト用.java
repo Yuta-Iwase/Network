493aaed9efd7c39b269dd3894a68b0a8f075d0d3
Yuta-Iwase <yutabk201@gmail.com>
2018-06-20 16:34:51 +0900

    fix

--- src/テスト用.java
+++ src/テスト用.java
@@ -1,11 +1,6 @@
 public class テスト用{
 	public static void main(String[] args) throws Exception{
-		NetworkForCSVFile net = new NetworkForCSVFile("WorldAir_w.csv", false, true);
-		net.setNode(false);
-		net.setEdge();
-		net.EdgeRewiring();
-
-		net.printList("re_world.csv");
+		System.out.println(1/(double)2);
 
 	}
 }
