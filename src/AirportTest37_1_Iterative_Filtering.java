8039c8132cc7e345447a0caa8b0fd59023229810
Owner <yutabk201@gmail.com>
2018-07-03 12:17:24 +0900

    write

--- src/AirportTest37_1_Iterative_Filtering.java
+++ src/AirportTest37_1_Iterative_Filtering.java
@@ -2,8 +2,16 @@
 public class AirportTest37_1_Iterative_Filtering {
 
 	public static void main(String[] args) {
-		ScaleFreeNetwork net = new ScaleFreeNetwork(1000, 2.7, 2, 500, 100);
-		net.printList();
+		int N = 1000;
+		double gamma = 2.7;
+		int minDegree = 2;
+
+		ScaleFreeNetwork net = new ScaleFreeNetwork(N, gamma, minDegree, N-1, 100);
+		net.setNode(false);
+		net.setEdge();
+		net.SetWeight_to_Alpha(0.2, N*100);
+
+		net.LinkSalience();
 	}
 
 }
