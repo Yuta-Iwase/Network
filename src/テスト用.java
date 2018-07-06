7a77bedffccbce65cf53c2e44d8a42c2c769e885
Yuta-Iwase <yutabk201@gmail.com>
2018-07-06 15:49:00 +0900

    set test

--- src/テスト用.java
+++ src/テスト用.java
@@ -1,13 +1,23 @@
+import java.io.File;
+import java.io.PrintWriter;
+
 public class テスト用{
 	public static void main(String[] args) throws Exception{
-		DMSNetwork net = new DMSNetwork(5, 1000, 2, 100);
-//		BarabasiAlbertNetwork net = new BarabasiAlbertNetwork(5, 1000, 2, 1L);
-		net.printList("C:\\users\\owner\\desktop\\netDNS.csv");
+		DMSNetwork DMS = new DMSNetwork(5, 1000, 2, 100);
+		BarabasiAlbertNetwork BA = new BarabasiAlbertNetwork(5, 1000, 2, 1L);
+//		net.printList("C:\\users\\owner\\desktop\\netDNS.csv");
 //		System.out.println(net.generateCount);
-		for(int i=0;i<net.M;i++) {
-			System.out.println(net.degree[net.list[i][0]] + "\t" + net.degree[net.list[i][1]]);
+		String desktop = "../../desktop/";
+		PrintWriter pw1 = new PrintWriter(new File(desktop + "DMS.csv"));
+		PrintWriter pw2 = new PrintWriter(new File(desktop + "BA.csv"));
+		for(int i=0;i<DMS.M;i++) {
+			pw1.println(DMS.degree[DMS.list[i][0]] + "," + DMS.degree[DMS.list[i][1]]);
 		}
-		
-//		net.printList();
+		for(int i=0;i<BA.M;i++) {
+			pw2.println(BA.degree[BA.list[i][0]] + "," + BA.degree[BA.list[i][1]]);
+		}
+
+		pw1.close();
+		pw2.close();
 	}
 }
