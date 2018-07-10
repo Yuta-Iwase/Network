25c08281b6283426701658ad90427c32264c0581
Owner <yutabk201@gmail.com>
2018-07-11 01:10:36 +0900

    test37_2

--- src/AirportTest37_2_Iterative_Filtering_for_DMS.java
+++ src/AirportTest37_2_Iterative_Filtering_for_DMS.java
@@ -1,60 +1,14 @@
-public class AirportTest37_2_Iterative_Filtering_for_DMS {
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AirportTest37_2_Iterative_Filtering_for_DMS extends Job{
 
 	public static void main(String[] args) throws Exception{
-		int times = 10;
+		AirportTest37_2_Iterative_Filtering_for_DMS job = new AirportTest37_2_Iterative_Filtering_for_DMS();
+		job.run("param.ini");
 
-		int N = 1000;
-		double gamma = 3.2;
-		int minDegree = 8;
-		double min_alpha = 0.5;
-		double delta_alpha = 0.1;
-		double alpha_t = 1;
-
-
-		double alpha = min_alpha;
-		HistogramGenerator hist = new HistogramGenerator();
-		double[][] sHist = null;
-
-		Network original_net = new ScaleFreeNetwork(N, gamma, minDegree, N-1, 100);
-
-		for(int f=0;f<alpha_t;f++) {
-			System.out.println("alpha\t" + alpha);
-
-			Network net = original_net.clone();
-			net.setNode(false);
-			net.setEdge();
-			net.SetWeight_to_Alpha(alpha, N*100);
-			net.disturb();
-
-			double hs_frac = -1.0;
-			for(int t=0;t<times;t++) {
-				final double INV_N = 1.0/N;
-//				final double INV_M = 1.0/net.M;
-
-				net.LinkSalience();
-				int[] sList = new int[net.edgeList.size()];
-				for(int i=0;i<net.edgeList.size();i++) sList[i]=net.edgeList.get(i).linkSalience;
-				sHist = hist.binPlot(sList, 20, false, 0, N);
-
-				hs_frac = (sHist[sHist.length-1][1]+sHist[sHist.length-2][1])*INV_N;
-				System.out.println("t=\t" + t  + "\thigh salience frac./N=\t" + hs_frac);
-				if(sHist[sHist.length-1][1]+sHist[sHist.length-2][1]==N) {
-					System.out.println("break as HS_frac=(N-1)/N");
-					break;
-				}
-
-				net = net.FilteringBySalience(0.0, 0.0);
-				net.setNode(false);
-				net.setEdge();
-				net.SetWeight_to_Alpha(alpha, N*100);
-				net.disturb();
-			}
-			net.printList("net2.csv");
-			System.out.println("hs_frac\t" + hs_frac);
-			System.out.println();
-
-			alpha += delta_alpha;
-		}
 	}
 
 	public static int k_max(Network input) {
@@ -63,6 +17,76 @@ public class AirportTest37_2_Iterative_Filtering_for_DMS {
 			if(max<input.degree.length) max=input.degree[i];
 		}
 		return max;
+	}
+
+	@Override
+	public void job(ArrayList<Object> controlParameterList) {
+		try {
+			int index = 0;
+
+			int N0 = Integer.parseInt(controlParameterList.get(index++).toString());
+			int N = Integer.parseInt(controlParameterList.get(index++).toString());
+			int insertEdges = Integer.parseInt(controlParameterList.get(index++).toString());
+			double a = Double.parseDouble(controlParameterList.get(index++).toString());
+
+			int times = Integer.parseInt(controlParameterList.get(index++).toString());
+
+			double inital_alpha = Double.parseDouble(controlParameterList.get(index++).toString());
+			double delta_alpha = Double.parseDouble(controlParameterList.get(index++).toString());
+			double alpha_t = Double.parseDouble(controlParameterList.get(index++).toString());
+
+			double alpha = inital_alpha;
+			HistogramGenerator hist = new HistogramGenerator();
+			double[][] sHist = null;
+
+			Network original_net = new DMSNetwork(N0, N, insertEdges, a, 100);
+
+			for(int f=0;f<alpha_t;f++) {
+				PrintWriter pw = new PrintWriter(new File("alpha=" + alpha + ".txt"));
+
+				System.out.println(new Date().toString());
+				System.out.println("alpha\t" + alpha);
+				System.out.println();
+
+				Network net = original_net.clone();
+				net.setNode(false);
+				net.setEdge();
+				net.SetWeight_to_Alpha(alpha, N*100);
+				net.disturb();
+
+				double hs_frac = -1.0;
+				for(int t=0;t<times;t++) {
+					final double INV_N = 1.0/N;
+//					final double INV_M = 1.0/net.M;
+
+					net.LinkSalience();
+					int[] sList = new int[net.edgeList.size()];
+					for(int i=0;i<net.edgeList.size();i++) sList[i]=net.edgeList.get(i).linkSalience;
+					sHist = hist.binPlot(sList, 20, false, 0, N);
+
+					hs_frac = (sHist[sHist.length-1][1]+sHist[sHist.length-2][1])*INV_N;
+					pw.println("t=\t" + t  + "\thigh salience frac./N=\t" + hs_frac);
+					if(sHist[sHist.length-1][1]+sHist[sHist.length-2][1]==N) {
+						System.out.println("break as HS_frac=(N-1)/N");
+						break;
+					}
+
+					net = net.FilteringBySalience(0.0, 0.0);
+					net.setNode(false);
+					net.setEdge();
+					net.SetWeight_to_Alpha(alpha, N*100);
+					net.disturb();
+				}
+
+				alpha += delta_alpha;
+
+				pw.close();
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+
+
 	}
 
 }
