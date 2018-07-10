2f83441eacf47316b230f01952d2fecec154df96
Owner <yutabk201@gmail.com>
2018-07-11 00:39:53 +0900

    new DMS net

--- src/DMSNetwork.java
+++ src/DMSNetwork.java
@@ -3,21 +3,44 @@ import java.util.ArrayList;
 public class DMSNetwork extends Network{
 	int generateCount = 0;
 
-	public DMSNetwork(int N0, int N, int insertEdges, int loopLimit) {
+	/**
+	 * DMS(Dorogovtsev, Mendes, Samukhin) networkを作る。<br>
+	 * 次数分布の指数は、3+a/insertEdges となる。<br>
+	 * @param N0 初期頂点数
+	 * @param N 最終的な頂点数
+	 * @param insertEdges 1回のステップに追加する辺の本数
+	 * @param a 頂点選択のときに各確率に履かせるゲタ
+	 * @param loopLimit 次数分布のリトライ回数
+	 */
+	public DMSNetwork(int N0, int N, int insertEdges, double a, int loopLimit) {
 		do {
 			// 初期化
 			degree = new int[N];
 
-			// 接続頂点決定判定用のリスト
-			ArrayList<Integer> stubList = new ArrayList<>();
+			// 確率の総和
+			double total_probability = 0.0;
+			// 追加スタブリスト
+			ArrayList<Integer> additionalStub_List = new ArrayList<>();
+			// 初期確率
+			final double INIT_VALUE = insertEdges+a;
+			// 初期確率総和
+			double total_init_prob = 0.0;
 
 			// 初期条件としてN0個の頂点を完全グラフで作成
-			for(int i=0;i<N0;i++) {
-				degree[i] = N0-1;
-				for(int j=0;j<N0-1;j++) {
-					stubList.add(i);
+			{
+				int N0_initStub = insertEdges;
+				int N0_additionalStub = (N0-1)-N0_initStub;
+				for(int i=0;i<N0;i++) {
+					degree[i] = N0-1;
+					for(int j=0;j<N0_additionalStub;j++) {
+						additionalStub_List.add(i);
+					}
 				}
+				total_probability = N0*(N0-1)+N0*a;
+				total_init_prob = N0*INIT_VALUE;
 			}
+
+
 
 			// BAモデル的な次数割り振りを行う
 			for(int i=N0;i<N;i++) {
@@ -25,8 +48,15 @@ public class DMSNetwork extends Network{
 
 				// 選択
 				for(int j=0;j<insertEdges;j++) {
-					int chosedStub = (int)(Math.random()*stubList.size());
-					int chosedNode = stubList.get(chosedStub);
+					double r = Math.random()*total_probability;
+					int chosedNode = -1;
+					if(r<=total_init_prob) {
+						chosedNode = (int)Math.round(r/INIT_VALUE);
+					}else {
+						int chosedStub = (int)(Math.random()*additionalStub_List.size());
+						chosedNode = additionalStub_List.get(chosedStub);
+					}
+
 					if(!currentChosedNodes.contains(chosedNode)) {
 						currentChosedNodes.add(chosedNode);
 					}else {
@@ -38,15 +68,16 @@ public class DMSNetwork extends Network{
 				// 選択頂点の情報更新
 				for(int j=0;j<currentChosedNodes.size();j++) {
 					int currentChosedNode = currentChosedNodes.get(j);
-					stubList.add(currentChosedNode);
+					additionalStub_List.add(currentChosedNode);
 					degree[currentChosedNode]++;
 				}
 
 				// 追加頂点の初期情報設定
 				degree[i] = insertEdges;
-				for(int j=0;j<insertEdges;j++) {
-					stubList.add(i);
-				}
+
+				// total_probability, sum_degreeの更新
+				total_probability += 2*insertEdges+a;
+				total_init_prob += INIT_VALUE;
 			}
 
 			// Configurationの要領でネットワークを構築
