d28ce5312c7f81e5c0f73996cb46e702d572e7bb
Owner <yutabk201@gmail.com>
2018-08-01 12:40:05 +0900

    marge

--- src/py_PointPlot.java
+++ src/py_PointPlot.java
@@ -249,7 +249,6 @@ public class py_PointPlot {
 		programPath_List.add("/usr/bin/python");
 		programPath_List.add("/usr/local/bin/python");
 
-
 		File pyFile = new File(pyFilePath);
 		PrintWriter pw = new PrintWriter(pyFile);
 
@@ -343,7 +342,10 @@ public class py_PointPlot {
 
 		argCodeList.add("");
 
-		temp = "function_List = [\"" + str_function + "\"]";
+		if(str_function.length()>0)
+			temp = "function_List = [\"" + str_function + "\"]";
+		else
+			temp = "function_List = []";
 		argCodeList.add(temp);
 
 		temp = "function_LineColors = [\"" + str_function_LineColors + "\"]";
@@ -355,91 +357,6 @@ public class py_PointPlot {
 
 		temp = "function_LegendLabel = [r\"" + str_function_LegendLabel + "\"]";
 		argCodeList.add(temp);
-
-//		String code2=
-//				"\r\n" +
-//				"### ここからプログラム内容\r\n" +
-//				"\r\n" +
-//				"import matplotlib.pyplot as plt\r\n" +
-//				"import pandas as pd\r\n" +
-//				"import os\r\n" +
-//				"\r\n" +
-//				"# データ読み取り\r\n" +
-//				"xList = []\r\n" +
-//				"yList = []\r\n" +
-//				"for i in range(len(inputPath)):\r\n" +
-//				"    # 読み取りDataFrame形式で返す\r\n" +
-//				"    readData = pd.read_csv(inputPath[i],header=None)\r\n" +
-//				"    # (DataFrame形式).value で縦横のラベルを剥がしてndarray形式に変更\r\n" +
-//				"    adarrayData = readData.values\r\n" +
-//				"    # x,yのデータへ分離\r\n" +
-//				"    adarrayDataX = adarrayData[:,0]\r\n" +
-//				"    adarrayDataY = adarrayData[:,1]\r\n" +
-//				"    # (ndarray形式).tolist() でndarrayをリスト形式へ変換\r\n" +
-//				"    x = adarrayDataX.tolist()\r\n" +
-//				"    xList.append(x)\r\n" +
-//				"    y = adarrayDataY.tolist()\r\n" +
-//				"    yList.append(y)\r\n" +
-//				"\r\n" +
-//				"# 累積分布の処理\r\n" +
-//				"if accumulationMode==1:\r\n" +
-//				"    for i in range(len(yList)):\r\n" +
-//				"        for j in range(len(yList[i])-1):\r\n" +
-//				"            yList[i][j+1] += yList[i][j]\r\n" +
-//				"if accumulationMode==2:\r\n" +
-//				"    for i in range(len(yList)):\r\n" +
-//				"        yList[i][0] = sum(yList[i]) - yList[i][0]\r\n" +
-//				"        for j in range(len(yList[i])-1):\r\n" +
-//				"            yList[i][j+1] = yList[i][j] - yList[i][j+1]\r\n" +
-//				"\r\n" +
-//				"# プロット\r\n" +
-//				"width = 0\r\n" +
-//				"if len(plotRangeX)>0:\r\n" +
-//				"    plt.xlim(plotRangeX)\r\n" +
-//				"if len(plotRangeY)>0:\r\n" +
-//				"    plt.ylim(plotRangeY)\r\n" +
-//				"if withLines:\r\n" +
-//				"    width = 2\r\n" +
-//				"if logscaleX:\r\n" +
-//				"    plt.xscale(\"log\")\r\n" +
-//				"if logscaleY:\r\n" +
-//				"    plt.yscale(\"log\")\r\n" +
-//				"plt.title(title)\r\n" +
-//				"plt.xlabel(xLabel)\r\n" +
-//				"plt.ylabel(yLabel)\r\n" +
-//				"\r\n" +
-//				"for i in range(len(inputPath)):\r\n" +
-//				"    if len(legendLabel)==len(inputPath):\r\n" +
-//				"        legend = legendLabel[i]\r\n" +
-//				"    else:\r\n" +
-//				"        legend = inputPath[i][:inputPath[i].find(\".\")]\r\n" +
-//				"    if len(dottedLine)>=len(inputPath) and withLines:\r\n" +
-//				"        if dottedLine[i]:\r\n" +
-//				"            style = \"--\"\r\n" +
-//				"        else:\r\n" +
-//				"            style = \"-\"\r\n" +
-//				"    else:\r\n" +
-//				"        style = \"\"\r\n" +
-//				"    if withPoints:\r\n" +
-//				"        plt.plot(xList[i],yList[i],linestyle=style,linewidth=width,color=lineColors[i],marker=\"o\",fillstyle=\"none\",markersize=pointSizes[i],markeredgecolor=pointColors[i],label=legend)\r\n" +
-//				"    else:\r\n" +
-//				"        plt.plot(xList[i],yList[i],linestyle=style,linewidth=width,color=lineColors[i],label=legend)\r\n" +
-//				"\r\n" +
-//				"if withLegend:\r\n" +
-//				"    if len(legendPosition)>0:\r\n" +
-//				"        plt.legend(loc = legendPosition)\r\n" +
-//				"    else:\r\n" +
-//				"        plt.legend()\r\n" +
-//				"\r\n" +
-//				"# 書き込み\r\n" +
-//				"if len(outputName)<=0:\r\n" +
-//				"    outputName = inputPath[0][:inputPath[0].find(\".csv\")]\r\n" +
-//				"if (not os.path.exists(\"output\")):\r\n" +
-//				"    os.makedirs(\"output\")\r\n" +
-//				"plt.savefig(\"output/\" + outputName + \".png\")\r\n" +
-//				"plt.savefig(\"output/\" + outputName + \".eps\", transparent=True)\r\n" +
-//				"plt.savefig(\"output/\" + outputName + \".pdf\", transparent=True)\r\n" +
-//				"";
 
 		String code2="### ここからプログラム内容\r\n" +
 				"\r\n" +
